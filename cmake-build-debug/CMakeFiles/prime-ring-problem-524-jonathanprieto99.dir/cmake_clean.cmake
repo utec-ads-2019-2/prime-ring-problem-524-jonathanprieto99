3e863c74a018c094f0bc8f8e1bcd691ad3a8039b
Jonathan Prieto <jonathan.prieto@me.com>
2019-10-16 17:20:42 -0500

    Upload Files

---
+++ cmake-build-debug/CMakeFiles/prime-ring-problem-524-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prime-ring-problem-524-jonathanprieto99.dir/main.cpp.o"
+  "prime-ring-problem-524-jonathanprieto99"
+  "prime-ring-problem-524-jonathanprieto99.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prime-ring-problem-524-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/prime-ring-problem-524-jonathanprieto99.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for prime-ring-problem-524-jonathanprieto99.
+# This may be replaced when dependencies are built.
